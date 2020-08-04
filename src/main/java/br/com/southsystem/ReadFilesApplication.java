16c30f054232664909509814e901c42f2d5b4613
Guilherme Costa <guilherme.costa@itbam.org.br>
2020-08-04 11:37:11 -0400

    create project

---
+++ src/main/java/br/com/southsystem/ReadFilesApplication.java
@@ -0,0 +1,13 @@
+package br.com.southsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReadFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReadFilesApplication.class, args);
+	}
+
+}
